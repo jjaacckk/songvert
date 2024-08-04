18004a1dd45dc29986f19b20edf8e7a64b7b493f
jjaacckk <8715589+jjaacckk@users.noreply.github.com>
2024-08-04 15:10:03 -0700

    removed 'track' from two errors to be broader

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-// research:
+// sources:
 // - https://doc.rust-lang.org/rust-by-example/error/multiple_error_types/wrap_error.html
 // - https://fettblog.eu/rust-enums-wrapping-errors/
 
@@ -8,8 +8,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    TrackMatchError,
-    TrackFindError,
+    MatchError,
+    FindError,
     SessionGrabError,
     RegexError(regex::Error),
     ParseError(serde_json::Error),
@@ -20,8 +20,8 @@ pub enum Error {
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::TrackMatchError => write!(f, "unable to find match"),
-            Error::TrackFindError => write!(f, "unable to find track(s) from given parameter(s)"),
+            Error::MatchError => write!(f, "unable to find match"),
+            Error::FindError => write!(f, "unable to find track(s) from given parameter(s)"),
             Error::SessionGrabError => write!(f, "unable to grab session info"),
             Error::RegexError(e) => write!(f, "{}", e),
             Error::ParseError(e) => write!(f, "{}", e),
@@ -34,7 +34,10 @@ impl Display for Error {
 impl std::error::Error for Error {
     // fn source(&self) -> Option<&(dyn std::error:Error + 'static)> {
     //     match *self {
-    //         Error::MatchError => None,
+    //         Error::TrackMatchError => None,
+    //         Error::TrackFindError => None,
+    //         Error::SessionGrabError => None,
+    //         Error::RegexError(ref e) => Some(e),
     //         Error::ParseError(ref e) => Some(e),
     //         Error::RetrievalError(ref e) => Some(e),
     //         Error::IoError(ref e) => Some(e)
