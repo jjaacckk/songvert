e0e52b724be0a0f065c653b1e612e4f6a8687596
jjaacckk <8715589+jjaacckk@users.noreply.github.com>
2024-08-05 23:49:01 -0700

    all service methods are now async and artist and album structs are renamed for simplicity and ean/upc added to album struct

--- src/service.rs
+++ src/service.rs
@@ -4,37 +4,58 @@ use crate::error::{Error, Result};
 use crate::spotify::Spotify;
 use crate::track::Track;
 use crate::youtube::YouTube;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Services {
     pub spotify: Option<Spotify>,
     pub apple_music: Option<AppleMusic>,
     pub youtube: Option<YouTube>,
     pub bandcamp: Option<Bandcamp>,
 }
+
 pub trait Service {
     // fn search_by_isrc(isrc: &str) -> Result<serde_json::Value, serde_json::Error>;
     // fn search_by_name(name: &str) -> Result<serde_json::Value, serde_json::Error>;
-    fn get_raw_track_match(track: &Track) -> Result<serde_json::Value>;
-    fn add_service_to_track(track: &mut Track) -> Result<()>;
-    fn create_service_from_raw(data: &serde_json::Value) -> Result<Self>
+    async fn get_raw_track_match(
+        client: &Client,
+        auth_token: &str,
+        track: &Track,
+    ) -> Result<serde_json::Value>;
+    async fn add_service_to_track(
+        client: &Client,
+        auth_token: &str,
+        track: &mut Track,
+    ) -> Result<()>;
+    async fn create_service_from_raw(data: &serde_json::Value) -> Result<Self>
     where
         Self: Sized;
-    fn create_track_from_id(id: &str) -> Result<Track>;
-    fn create_track_from_raw(data: &serde_json::Value) -> Result<Track>;
-    fn create_tracks_from_playlist_id(id: &str) -> Result<Vec<Track>>;
-    fn create_tracks_from_playlist_raw(data: &serde_json::Value) -> Result<Vec<Track>>;
+    async fn create_track_from_id(
+        client: &Client,
+        auth_token: &str,
+        track_id: &str,
+    ) -> Result<Track>;
+    async fn create_track_from_raw(data: &serde_json::Value) -> Result<Track>;
+    async fn create_tracks_from_playlist_id(
+        client: &Client,
+        auth_token: &str,
+        playlist_id: &str,
+    ) -> Result<Vec<Track>>;
+    async fn create_tracks_from_playlist_raw(data: &serde_json::Value) -> Result<Vec<Track>>;
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ArtistOnService {
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Artist {
     pub id: String,
     pub name: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct AlbumOnService {
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct Album {
     pub id: String,
     pub name: String,
+    pub total_tracks: u8,
+    pub ean: Option<String>,
+    pub upc: Option<String>,
 }
