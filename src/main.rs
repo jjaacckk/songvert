0531fff286d36f50d6868a13d6f4ade4e982e9b7
jjaacckk <8715589+jjaacckk@users.noreply.github.com>
2024-09-22 12:19:22 -0700

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
-// // use anyhow::{Context, Result};
-// use clap::{ArgGroup, Parser};
-// use std::path::PathBuf;
-//
-
-mod apple_music;
-mod bandcamp;
-mod error;
-mod playlist;
-mod service;
-mod spotify;
-mod track;
-mod utils;
-mod youtube;
-
-use apple_music::AppleMusic;
-use bandcamp::Bandcamp;
-use error::{Error, Result};
-use playlist::Playlist;
 use reqwest::Client;
-use service::{Album, Artist, Services};
-use spotify::{SessionInfo, Spotify};
-use std::io::Write;
-use track::Track;
-use youtube::{Payload, YouTube};
+use songvert::{
+    apple_music::AppleMusic,
+    error::Result,
+    playlist::Playlist,
+    spotify::{SessionInfo, Spotify},
+};
+
+mod cli;
+use crate::cli::Cli;
+use clap::Parser;
 
 fn spotify_playlist_conversion_sanity_check(playlist: &Playlist) -> Result<()> {
     let mut youtube_miss_count = 0;
@@ -101,6 +87,7 @@ async fn show_downloader(
                     playlist_file_path,
                 )
                 .await?;
+                println!("Attempting to match {} tracks:", playlist.tracks.len());
                 playlist
                     .add_apple_music(&client, AppleMusic::PUBLIC_BEARER_TOKEN)
                     .await?;
@@ -116,8 +103,9 @@ async fn show_downloader(
     let middle = std::time::Instant::now();
 
     println!(
-        "Playlist Match/Load took {} seconds",
-        (middle - start).as_secs()
+        "Playlist Match/Load took {} seconds ({} ms)",
+        (middle - start).as_secs(),
+        (middle - start).as_millis()
     );
 
     println!("");
@@ -136,13 +124,20 @@ async fn show_downloader(
 
     let end = std::time::Instant::now();
 
-    println!(
-        "Playlist Download took {} seconds",
-        (end - middle).as_secs()
-    );
+    if download_tracks == true {
+        println!(
+            "Playlist Download took {} seconds  ({} ms)",
+            (end - middle).as_secs(),
+            (end - middle).as_millis()
+        );
+    }
 
     println!("");
-    println!("Total time: {} seconds", (end - start).as_secs());
+    println!(
+        "Total time: {} seconds ({} ms)",
+        (end - start).as_secs(),
+        (end - start).as_millis()
+    );
     println!("\n----------\nEnd.");
 
     Ok(playlist)
@@ -152,47 +147,35 @@ async fn show_downloader(
 async fn main() -> Result<()> {
     std::env::set_var("RUST_BACKTRACE", "full");
 
+    //let cli = Cli::parse();
+
     let client: Client = reqwest::Client::builder()
             .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15")
             .build()?;
 
-    let test_oct_15_2023_playlist_id = "6lJfNdZUnat3Qa4kKaVIxG";
-    let filename = "tester_oct_15_2023";
+    let playlist_id = "4v4JZarNXGgy2JbKqOnIws";
+    let filename = "show_71_sep_22_2024";
 
-    let playlist = show_downloader(&client, test_oct_15_2023_playlist_id, filename, true).await?;
+    let mut playlist = show_downloader(&client, playlist_id, filename, false).await?;
 
-    // let image_bytes = utils::download_image_bytes(
-    //     &client,
-    //     // "https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg?resize=2048",
-    //     "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-photo%2Flifelike-cheese-squares-resembling_1002329-375.jpg&f=1&nofb=1&ipt=f04db5d61ae98b00867aab0128ccf07898156b32afe99017764c623fc0c10c32&ipo=images",
-    //     Some(AppleMusic::PUBLIC_BEARER_TOKEN),
-    // )
-    // .await?;
+    //playlist.tracks[15]
+    //    .download(&client, "./", "cheeser", true)
+    //    .await?;
 
-    // playlist.tracks[0]
-    //     .download(&client, "./", "cheese", true)
-    //     .await?;
+    //let track = &mut playlist.tracks[15];
+    //match track.add_youtube(&client).await {
+    //    Ok(..) => println!("success: {}", serde_json::to_string_pretty(&track)?),
+    //    Err(e) => println!("failure: {}", e),
+    //}
 
-    // utils::add_artwork_to_mp3(image_bytes, &x, true)?;
-    // println!("{}", x);
-    // let mut tracks: Vec<Track> = serde_json::from_str(&std::fs::read_to_string("./test.json")?)?;
-    // let track: &mut Track = &mut tracks[3];
-
-    // match track
-    //     .services
-    //     .youtube
-    //     .as_ref()
-    //     .ok_or(Error::DownloadError)?
-    //     .download(
-    //         &client,
-    //         &format!("{} - {}", &track.name, &track.artists.join(", ")),
-    //         "./test_downloads/",
-    //     )
-    //     .await
-    // {
-    //     Ok(..) => println!("done."),
-    //     Err(e) => println!("{}", e),
-    // }
+    //playlist.tracks[18]
+    //    .download(
+    //        &client,
+    //        "./example_show_playlists/downloads/",
+    //        "cheese",
+    //        true,
+    //    )
+    //    .await?;
 
     Ok(())
 }
