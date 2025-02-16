3d7bc9529f53bd82a930f31eba742a7f3147b74b
jjaacckk <8715589+jjaacckk@users.noreply.github.com>
2025-02-16 14:34:12 -0800

    fixed pub API key

--- src/apple_music.rs
+++ src/apple_music.rs
@@ -207,7 +207,7 @@ impl AppleMusic {
     pub const API_BASE_URL: &'static str = "https://api.music.apple.com/v1";
     pub const SITE_BASE_URL: &'static str = "https://music.apple.com";
 
-    pub const PUBLIC_BEARER_TOKEN: &'static str = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNzMxMDgzODA4LCJleHAiOjE3MzgzNDE0MDgsInJvb3RfaHR0cHNfb3JpZ2luIjpbImFwcGxlLmNvbSJdfQ.elRh3AWbAD-Qs_8dNfDCiP3tIObHlUfRsT276Op5aqkWfp1JRwNbbhcWYpoWzAc5mRjW1m-X2UwwuPp-0rYr9Q";
+    pub const PUBLIC_BEARER_TOKEN: &'static str = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNzM3NDgwNzIxLCJleHAiOjE3NDQ3MzgzMjEsInJvb3RfaHR0cHNfb3JpZ2luIjpbImFwcGxlLmNvbSJdfQ.n_2fV0lbEYcLIKyt590X3A0oH8VFugS53cmTFiGHYxy1ilDRd-rWp9K1Ka2r5aAf-cRdsMaHx7VZSJ4IwG9UnQ";
 
     async fn get(client: &Client, auth: &str, path: &str) -> Result<Value> {
         let request: RequestBuilder = client
