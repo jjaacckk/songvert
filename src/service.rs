4aa7c363755a6fbfca8811a756d651ea25b0cdf3
jjaacckk <8715589+jjaacckk@users.noreply.github.com>
2025-05-25 15:28:48 -0700

    clean up

--- src/service.rs
+++ src/service.rs
@@ -1,10 +1,7 @@
 use crate::apple_music::AppleMusic;
 use crate::bandcamp::Bandcamp;
-// use crate::error::Result;
 use crate::spotify::Spotify;
-// use crate::track::{Playlist, Track};
 use crate::youtube::YouTube;
-// use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
@@ -20,25 +17,6 @@ pub enum Source {
     Spotify,
     AppleMusic,
 }
-
-// pub trait Service<'a> {
-//     const API_BASE_URL: &'static str;
-//     const SITE_BASE_URL: &'static str;
-//     // async fn get_raw_track_match_from_track(
-//     //     client: &Client,
-//     //     track: &Track,
-//     // ) -> Result<serde_json::Value>;
-// async fn create_service_for_track(client: &Client, track: &mut Track) -> Result<()>;
-// async fn create_service_from_raw<T>(raw_track: &'a T) -> Result<Self>
-// where
-//     Self: Sized;
-//     // async fn create_track_from_id(client: &Client, track_id: &str) -> Result<Track>;
-// async fn create_track_from_raw(raw_track: &'a impl serde::Deserialize<'a>) -> Result<Track>;
-//     // async fn create_playlist_from_id(client: &Client, playlist_id: &str) -> Result<Playlist>;
-// async fn create_playlist_from_raw(
-//     raw_playlist: &'a Vec<impl serde::Deserialize>,
-// ) -> Result<Playlist>;
-// }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct Artist {
